a5d5432f0b66c25c299a9cfd74eaf0a1e350de09
Nemo <nemonapatsadon@gmail.com>
2026-02-14 02:53:58 +0700

    18-2

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 struct Rect{
-	double x,y,w,h;
+    double x, y, w, h;
 };
 
-double overlap(){
+double overlap(Rect *r1, Rect *r2){
 
-
-
+    double x_overlap = min(r1->x + r1->w, r2->x + r2->w) - max(r1->x, r2->x);
+    double y_overlap = min(r1->y, r2->y) - max(r1->y - r1->h, r2->y - r2->h);
+    if(x_overlap <= 0 || y_overlap <= 0) return 0;
+    
+    return x_overlap * y_overlap;
 }
