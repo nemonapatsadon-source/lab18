8d82ff8dcde7f20d918cd685ecc8573e042453e0
Nemo <nemonapatsadon@gmail.com>
2026-02-14 02:53:29 +0700

    18-1

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,20 @@
 #include<iostream>
+#include<algorithm> // ต้อง include เพื่อใช้ max และ min
 using namespace std;
 
-struct Rect{
-	double x,y,w,h;
+struct Rect {
+    double x, y, w, h;
 };
 
-double overlap(){
+double overlap(Rect r1, Rect r2) {
 
+    double x_overlap = min(r1.x + r1.w, r2.x + r2.w) - max(r1.x, r2.x);
+    double y_overlap = min(r1.y, r2.y) - max(r1.y - r1.h, r2.y - r2.h);
 
+    if (x_overlap <= 0 || y_overlap <= 0) {
+        return 0;
+    }
 
+    return x_overlap * y_overlap;
 }
+
